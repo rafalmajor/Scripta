f3f0559a784c7aa11572149ee7f9826f29b53cac
rafal <rafal.major@gmail.com>
2025-02-08 20:42:04 +0100

    add new first view

---
+++ scr/Scripta.UI.WPF/MainView.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace Scripta.UI.WPF;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
