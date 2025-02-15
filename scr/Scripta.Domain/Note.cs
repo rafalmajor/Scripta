fa7eeefabc36b6a45e36c25f7bac12f7691f39cd
rafal <rafal.major@gmail.com>
2025-02-15 21:40:30 +0100

    Remove build errors

--- scr/Scripta.Domain/Note.cs
+++ scr/Scripta.Domain/Note.cs
@@ -3,6 +3,6 @@
 public class Note
 {
     public Guid Id { get; init; }
-    public string Title { get; set; }
-    public string Content { get; set; }
+    public required string Title { get; set; }
+    public required string Content { get; set; }
 }
