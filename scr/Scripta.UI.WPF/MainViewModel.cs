883b55ec7ccd8b6b8f2d8a0424312fa61b1d1a4a
rafal <rafal.major@gmail.com>
2025-02-17 20:12:50 +0100

    Use Note in the view

--- scr/Scripta.UI.WPF/MainViewModel.cs
+++ scr/Scripta.UI.WPF/MainViewModel.cs
@@ -1,17 +1,33 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using System.Windows;
+using Scripta.Domain;
 
 namespace Scripta.UI.WPF;
 
 public partial class MainViewModel : ObservableObject
 {
+    private Note _note = new Note
+    {
+        Id = Guid.NewGuid(),
+        Title = "Note 1",
+        Content = "This is a note 1"
+    };
+    
     [ObservableProperty]
     private string _message;
+    
+    [ObservableProperty]
+    private string _title;
+    
+    [ObservableProperty]
+    private string _description;
 
     public MainViewModel()
     {
         Message = "Hello, World!";
+        Title = _note.Title;
+        Description = _note.Content;
     }
     
     [RelayCommand]
