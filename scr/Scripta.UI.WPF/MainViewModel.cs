78d14c009c15c93159eb752be6b5d08fcee47b29
rafal <rafal.major@gmail.com>
2025-02-19 21:06:26 +0100

    Add list of notes

--- scr/Scripta.UI.WPF/MainViewModel.cs
+++ scr/Scripta.UI.WPF/MainViewModel.cs
@@ -1,5 +1,6 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
 using System.Windows;
 using Scripta.Domain;
 
@@ -13,19 +14,30 @@ public partial class MainViewModel : ObservableObject
         Title = "Note 1",
         Content = "This is a note 1"
     };
-   
+
     [ObservableProperty]
     private string _title;
-    
+
     [ObservableProperty]
     private string _description;
+
+    [ObservableProperty]
+    private ObservableCollection<Note> _notes;
 
     public MainViewModel()
     {
         Title = _note.Title;
         Description = _note.Content;
+
+        // Initialize the notes collection
+        Notes = new ObservableCollection<Note>
+        {
+            new Note { Id = Guid.NewGuid(), Title = "Note 1", Content = "This is note 1" },
+            new Note { Id = Guid.NewGuid(), Title = "Note 2", Content = "This is note 2" },
+            new Note { Id = Guid.NewGuid(), Title = "Note 3", Content = "This is note 3" }
+        };
     }
-    
+
     [RelayCommand]
     private void ShowMessage()
     {
